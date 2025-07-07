9e2aaa1c03fcfe59974c6bc8c6622f6002c081b9
SamanKeighobadi <saman.keighobadi@gmail.com>
2025-07-07 16:04:18 +0330

    feat: install zod resolver for validation & react query
    - create query client provider wrapper component as client component which can wrapp in layout server side component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { QueryClientProviderWrapper } from "./components/QueryClientProviderWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <QueryClientProviderWrapper>{children}</QueryClientProviderWrapper>
       </body>
     </html>
   );
