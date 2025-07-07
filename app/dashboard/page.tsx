bf47badbf2a5414cd732266a7317d6cd791c7ac8
SamanKeighobadi <saman.keighobadi@gmail.com>
2025-07-07 12:55:10 +0330

    feat: install required styling packages like sass & bootstrap , create dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const DashbaordPage = () => {
+  return (
+    <div className=" d-flex  justify-content-center ">
+      <div className="alert alert-info">
+        <h1 className="p-4 ">Wellcom To Dashbaord !</h1>
+      </div>
+    </div>
+  );
+};
+
+export default DashbaordPage;
